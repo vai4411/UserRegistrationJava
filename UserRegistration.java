5002cb2c8b6dd744aa71f02a9e005f5d94e6f89d
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-04-19 19:05:37 +0530

    ADDED: Check Valid Email Address

--- UserRegistration.java
+++ UserRegistration.java
@@ -9,4 +9,8 @@ public class UserRegistration {
     public boolean lastName(Pattern pattern, String name) {
         return pattern.matches(String.valueOf(pattern),name);
     }
+
+    public boolean email(Pattern pattern, String emailAddress) {
+        return pattern.matches(String.valueOf(pattern),emailAddress);
+    }
 }
--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -7,14 +7,21 @@ public class UserRegistrationTest {
     @Test
     public void First_Name_Check() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.firstName(Pattern.compile("[A-Z][A-Za-z]{2,}"), "Abc");
+        boolean result = userRegistration.firstName(Pattern.compile("^[A-Z][A-Za-z]{2,}"), "Abc");
         Assert.assertEquals(true,result);
     }
 
     @Test
     public void Last_Name_Check() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.lastName(Pattern.compile("[A-Z][A-Za-z]{2,}"),"Def");
+        boolean result = userRegistration.lastName(Pattern.compile("^[A-Z][A-Za-z]{2,}"),"Def");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void Email_Check() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.email(Pattern.compile("^[0-9a-z]+[@][a-z]+[.]{1}[a-z]{2,}"),"vai123@gmail.com");
         Assert.assertEquals(true,result);
     }
 }
