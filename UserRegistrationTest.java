d4979d374845d09b7dc863b6c290683be3ec65c5
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-04-19 19:40:30 +0530

    ADDED: Password Contains Atleast 1 UpperCase

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -23,7 +23,7 @@ public class UserRegistrationTest {
     @Test
     public void Email_Check() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.email(Pattern.compile("^[0-9a-z]+[@][a-z]+[.]{1}[a-z]{2,}"),"vai123@gmail.com");
+        boolean result = userRegistration.email(Pattern.compile("^[0-9a-z]+[@][a-z]+[.][a-z]{2,}"),"vai123@gmail.com");
         Assert.assertEquals(true,result);
     }
 
@@ -37,7 +37,91 @@ public class UserRegistrationTest {
     @Test
     public void Password_Check() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean result = userRegistration.password(Pattern.compile("^[A-Za-z0-9]{8,}"),"aBcD1234");
+        boolean result = userRegistration.password(Pattern.compile("(?=.*[A-Z].*)[A-Za-z0-9]{8,}"),"AbCd1234");
+        Assert.assertEquals(true,result);
+    }
+}import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationTest {
+
+    @Test
+    public void First_Name_Check() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.firstName(Pattern.compile("^[A-Z][A-Za-z]{2,}"), "Abc");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void Last_Name_Check() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.lastName(Pattern.compile("^[A-Z][A-Za-z]{2,}"),"Def");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void Email_Check() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.email(Pattern.compile("^[0-9a-z]+[@][a-z]+[.][a-z]{2,}"),"vai123@gmail.com");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void Mobile_Number_Check() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.mobileNumber(Pattern.compile("^[0-9]{2}[ ][0-9]{10}"),"91 1234567890");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void Password_Check() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.password(Pattern.compile("(?=.*[A-Z].*)[A-Za-z0-9]{8,}"),"AbCd1234");
+        Assert.assertEquals(true,result);
+    }
+}import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistrationTest {
+
+    @Test
+    public void First_Name_Check() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.firstName(Pattern.compile("^[A-Z][A-Za-z]{2,}"), "Abc");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void Last_Name_Check() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.lastName(Pattern.compile("^[A-Z][A-Za-z]{2,}"),"Def");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void Email_Check() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.email(Pattern.compile("^[0-9a-z]+[@][a-z]+[.][a-z]{2,}"),"vai123@gmail.com");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void Mobile_Number_Check() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.mobileNumber(Pattern.compile("^[0-9]{2}[ ][0-9]{10}"),"91 1234567890");
+        Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void Password_Check() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.password(Pattern.compile("(?=.*[A-Z].*)[A-Za-z0-9]{8,}"),"AbCd1234");
         Assert.assertEquals(true,result);
     }
 }
